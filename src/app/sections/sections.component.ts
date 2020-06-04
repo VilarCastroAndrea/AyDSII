ad89ad38b115c4e8f5f51832530cd8c762ad6ddd
andi <andiipuckett@gmail.com>
2020-06-04 01:11:20 -0300

    version1

---
+++ src/app/sections/sections.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sections',
+  templateUrl: './sections.component.html',
+  styleUrls: ['./sections.component.css']
+})
+
+export class SectionsComponent implements OnInit {
+  focus;
+  focus1;
+
+  ngOnInit() {
+  }
+
+  constructor() {
+
+  }
+
+}
