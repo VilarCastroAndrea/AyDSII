85aa5a4a66a5a0c2d441be9e5adc878685827334
andi <andiipuckett@gmail.com>
2020-06-23 21:26:53 -0300

    version 1

--- src/app/sections/sections.component.ts
+++ src/app/sections/sections.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-sections',
   templateUrl: './sections.component.html',
@@ -10,11 +12,45 @@ export class SectionsComponent implements OnInit {
   focus;
   focus1;
 
+  closeResult: string;
+
   ngOnInit() {
   }
 
-  constructor() {
+  constructor(private modalService: NgbModal) {
 
   }
+
+  open(content, type, modalDimension) {
+    if (modalDimension === 'sm' && type === 'modal_mini') {
+        this.modalService.open(content, { windowClass: 'modal-mini', size: 'sm', centered: true }).result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    } else if (modalDimension === '' && type === 'Notification') {
+        this.modalService.open(content, { windowClass: 'modal-danger', centered: true }).result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    } else {
+        this.modalService.open(content, { centered: true }).result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+}
+
+private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+        return 'by clicking on a backdrop';
+    } else {
+        return `with: ${reason}`;
+    }
+}
 
 }
